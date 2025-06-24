218eab33ab454e3ed7a5bcd36dfe1a85bf5bf522
zoz312 <hdhyfhkhaldhswkhh@gmail.com>
2025-06-24 20:36:17 +0300

    delete

--- lib/features/auth/profile/data/repo/profile_repo.dart
+++
@@ -1,9 +0,0 @@
-
-
-import 'package:dartz/dartz.dart';
-import 'package:quranapp/core/errors/failure.dart';
-import 'package:quranapp/features/auth/profile/data/models/profile_model.dart';
-
-abstract class ProfileRepo {
-  Future<Either<Failure, UserModel>> getProfile(int id);
-}
