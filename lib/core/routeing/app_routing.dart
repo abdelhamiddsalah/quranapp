ced1f3c5d7daa5255f37abb53f9bbcb371cffb5b
zoz312 <hdhyfhkhaldhswkhh@gmail.com>
2025-07-07 15:03:44 +0300

    w

--- lib/core/routeing/app_routing.dart
+++ lib/core/routeing/app_routing.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:quranapp/core/routeing/routes.dart';
-import 'package:quranapp/depency_injection.dart';
 import 'package:quranapp/features/auth/login/presention/views/login_screen.dart';
-import 'package:quranapp/features/auth/profile/presention/manger/cubit/profile_cubit.dart';
-import 'package:quranapp/features/auth/profile/presention/views/profile_view.dart';
+
 import 'package:quranapp/features/auth/signup/presention/views/signup.dart';
 import 'package:quranapp/features/auth/welcome/presention/welcome_view.dart';
 import 'package:quranapp/features/home/presentation/view/home.dart';
 import 'package:quranapp/features/home/presentation/view/search_view.dart';
 import 'package:quranapp/features/home/presentation/view/widgets/bottom_nav.dart';
-import 'package:quranapp/features/home/presentation/view/widgets/profile.dart';
 
 final GoRouter router = GoRouter(
   routes: [
@@ -33,16 +29,7 @@ final GoRouter router = GoRouter(
       path: Routes.bottomnav,
       builder: (context, state) => const MyHomePage(),
     ),
-    GoRoute(
-      path: '/profile',
-      builder: (context, state) {
-     
-        return BlocProvider(
-          create: (context) => sl<ProfileCubit>()..getProfile(),
-          child: const ProfileScreen(),
-        );
-      },
-    ),
+
   ],
   errorBuilder: (context, state) =>
       const Scaffold(body: Center(child: Text('Page not found'))),
