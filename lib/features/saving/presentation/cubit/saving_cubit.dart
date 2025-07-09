d11cf9663de49559be387af120f3c9048948c6a7
zoz312 <hdhyfhkhaldhswkhh@gmail.com>
2025-07-09 18:16:48 +0300

    delete saving

--- lib/features/saving/presentation/cubit/saving_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'saving_state.dart';
-
-class SavingCubit extends Cubit<SavingState> {
-  SavingCubit() : super(SavingInitial());
-}
